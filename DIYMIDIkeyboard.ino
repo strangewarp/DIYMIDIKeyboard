a1e889647958a3370ebe7278aa49be9a17fbf793
C.D. Madsen <sevenplagues@gmail.com>
2012-12-29 00:23:09 -0700

    debugging
    Fixed an extremely annoying bug. Turns out the official Arduino Keypad
    library treats a key of 0 as a NO_KEY! Blarg I am dead.
    
    Also changed the saveload toggle button into a standalone toggle switch,
    instead of confusingly incorporating it into the butkey matrix.
    
    Also reduced the number of preset buttons in butkeypad to 9, of which
    I'll be using 8.
    
    Finally, spiffed up the PWM action on the save/load LEDs.

--- DIYMIDIkeyboard.ino
+++ DIYMIDIkeyboard.ino
@@ -1,20 +1,19 @@
 
 #include <EEPROM.h> // http://arduino.cc/en/Reference/EEPROM
-#include <Keypad.h> // http://playground.arduino.cc//Code/Keypad
+#include <Keypad.h> // https://github.com/Nullkraft/Keypad
 #include <Potentiometer.h> // http://playground.arduino.cc//Code/Potentiometer
 
-#define CONFIG_VERSION "dk1"
+#define CONFIG_VERSION "dk4"
 #define CONFIG_START 32
 
 struct settings {
-  char data[16][4];
+  byte data[9][4];
   char vers[4];
 } saves = {
   {
-    {2,0,1,127}, {2,1,1,127}, {2,2,1,127}, {2,3,1,127},
-    {2,4,1,127}, {2,5,1,127}, {2,6,1,127}, {2,7,1,127},
-    {2,8,1,127}, {2,9,1,127}, {2,10,1,127}, {2,11,1,127},
-    {2,12,1,127}, {2,13,1,127}, {2,14,1,127}, {2,15,1,127}
+    {2,0,1,127}, {2,1,1,127}, {2,2,1,127},
+    {2,4,1,127}, {2,5,1,127}, {2,6,1,127},
+    {2,8,1,127}, {2,9,1,127}, {2,10,1,127}
   },
   CONFIG_VERSION
 };
@@ -27,8 +26,8 @@ const byte butcols = 4;
 
 byte rowpins[rows] = {23, 25, 27, 29};
 byte colpins[cols] = {39, 41, 43, 45, 47, 49, 51, 53};
-byte butrowpins[4] = {38, 40, 42, 44};
-byte butcolpins[4] = {46, 48, 50, 52};
+byte butrowpins[3] = {38, 40, 42};
+byte butcolpins[3] = {48, 50, 52};
 byte ledpins[8] = {22, 24, 26, 28, 30, 32, 34, 36};
 byte saveledpin = 2;
 byte loadledpin = 3;
@@ -48,24 +47,26 @@ byte commandbus = 0;
 byte velocitybus = 0;
 
 int saveload = LOW; // SAVE-LOAD mode tracking variable. LOW for save mode, HIGH for load mode. Set by digitalRead of slswitchpin
+int slnew = LOW; // Is used to compare the new saveload pin value to the previous iteration's saveload pin value
 byte butactive = 0;
 byte adjbutkey = 0;
-byte pulseval = 0;
+byte adjnotekey = 0;
+int pulseval = 0;
 int pulsedelay = 20;
-long prevmillis = 0;
+int pulseinc = 5;
+long prevmillis = millis();
 
 char keys[rows][cols] = {
-  { 0, 1, 2, 3, 4, 5, 6, 7},
-  { 8, 9,10,11,12,13,14,15},
-  {16,17,18,19,20,21,22,23},
-  {24,25,26,27,28,29,30,31}
+  { 1, 2, 3, 4, 5, 6, 7, 8},
+  { 9,10,11,12,13,14,15,16},
+  {17,18,19,20,21,22,23,24},
+  {25,26,27,28,29,30,31,32}
 };
 
 char butkeys[butrows][butcols] = {
-  { 0, 1, 2, 3},
-  { 4, 5, 6, 7},
-  { 8, 9,10,11},
-  {12,13,14,15}
+  { 1, 2, 3},
+  { 4, 5, 6},
+  { 7, 8, 9}
 };
 
 Keypad keypad = Keypad(makeKeymap(keys), rowpins, colpins, rows, cols);
@@ -150,21 +151,23 @@ void loop() {
     setBinaryLEDs(velocity);
   }
   
-  saveload = digitalRead(slswitchpin);
-  if (saveload == LOW) { // Light up SAVE-LED
-    pulseval = 150;
-    analogWrite(saveledpin, 150);
-    analogWrite(loadledpin, 0);
-  } else if (saveload == HIGH) { // Light up LOAD-LED
-    pulseval = 150;
-    analogWrite(saveledpin, 0);
-    analogWrite(loadledpin, 150);
+  slnew = digitalRead(slswitchpin);
+  
+  if (saveload != slnew) {
+    saveload = slnew;
+    if (saveload == LOW) { // Light up SAVE-LED
+      analogWrite(saveledpin, pulseval);
+      analogWrite(loadledpin, 0);
+    } else if (saveload == HIGH) { // Light up LOAD-LED
+      analogWrite(saveledpin, 0);
+      analogWrite(loadledpin, pulseval);
+    }
   }
 
   butactive = butkeypad.getKey();
   
   if (butkeypad.kstate == PRESSED) {
-    if (butkeypad.stateChanged) {
+    if (butactive != NO_KEY) {
       
       for (int kdnum = 0; kdnum < (rows * cols); kdnum++) {
         if (keydown[kdnum] == true) {
@@ -172,42 +175,53 @@ void loop() {
           if (command == 1) {
             noteSend(
               channel + 128,
-              (keypad.key[kdnum].kchar + (octave * 12)) % 128,
+              ((keypad.key[kdnum].kchar - 1) + (octave * 12)) % 128,
               velocity
             );
           }
         }
       }
       
+      adjbutkey = butkeypad.kchar - 1;
       if (saveload == LOW) {
-        saves.data[butkeypad.kchar][0] = octave;
-        saves.data[butkeypad.kchar][1] = channel;
-        saves.data[butkeypad.kchar][2] = command;
-        saves.data[butkeypad.kchar][3] = velocity;
+        saves.data[adjbutkey][0] = octave;
+        saves.data[adjbutkey][1] = channel;
+        saves.data[adjbutkey][2] = command;
+        saves.data[adjbutkey][3] = velocity;
         saveConfig();
       } else if (saveload == HIGH) {
-        octave = saves.data[butkeypad.kchar][0];
-        channel = saves.data[butkeypad.kchar][1];
-        command = saves.data[butkeypad.kchar][2];
-        velocity = saves.data[butkeypad.kchar][3];
+        octave = saves.data[adjbutkey][0];
+        channel = saves.data[adjbutkey][1];
+        command = saves.data[adjbutkey][2];
+        velocity = saves.data[adjbutkey][3];
       }
       octavebus = octavecheck;
       channelbus = channelcheck;
       commandbus = commandcheck;
       velocitybus = velocitycheck;
-      setBinaryLEDs(adjbutkey);
+      setBinaryLEDs(butactive);
       
     }
   }
   
   if ((millis() - pulsedelay) > prevmillis) { // Adjust the saveload LED's PWM pulsation, based on timing variables
+  
     prevmillis = millis();
-    pulseval = (pulseval + 2) % 255;
+    
+    pulseval += pulseinc;
+    if ((pulseval < 0)
+    || (pulseval > 255)
+    ) {
+      pulseinc *= -1;
+      pulseval = max(0, min(255, pulseval));
+    }
+    
     if (saveload == LOW) { // Write the pulse value to SAVE-LED
       analogWrite(saveledpin, pulseval);
     } else if (saveload == HIGH) { // Write the pulse value to LOAD-LED
       analogWrite(loadledpin, pulseval);
     }
+    
   }
   
   keypad.getKeys();
@@ -216,6 +230,8 @@ void loop() {
     
     if (keypad.key[i].kchar) {
       
+      adjnotekey = keypad.key[i].kchar - 1;
+      
       if (keypad.key[i].kstate == PRESSED) {
         
         if (keypad.key[i].stateChanged) {
@@ -223,10 +239,10 @@ void loop() {
             keydown[i] = true;
             noteSend(
               channel + (command * 16) + 128,
-              (keypad.key[i].kchar + (octave * 12)) % 128,
+              (adjnotekey + (octave * 12)) % 128,
               velocity
             );
-            setBinaryLEDs((keypad.key[i].kchar + (octave * 12)) % 128);
+            setBinaryLEDs((adjnotekey + (octave * 12)) % 128);
           }
         }
         
@@ -238,7 +254,7 @@ void loop() {
             if (command == 1) {
               noteSend(
                 channel + 128,
-                (keypad.key[i].kchar + (octave * 12)) % 128,
+                (adjnotekey + (octave * 12)) % 128,
                 velocity
               );
             }
